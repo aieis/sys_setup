77a6e14e67abc14fb5f01467d9533ecc9aaa582f
Ahmed Salem <aieisalem17@gmail.com>
2023-02-15 17:08:01 +0000

    Add git install fix alias error

--- setup_tx2.sh
+++ setup_tx2.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
 #install emacs
-sudo add-apt-repository ppa:kelleyk/emacs
+sudo add-apt-repository -y ppa:kelleyk/emacs
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
-sudo apt install emacs28
+sudo apt install -y emacs28
+sudo apt install -y git
 
 git clone https://github.com/aieis/doom.d ~/.doom.d
 git clone https://github.com/doomemacs/doomemacs.git ~/.emacs.d
 
 ~/.emacs.d/bin/doom install --force
 
-echo "alias ec=emacsclient -n -c -a ''" >> ~/.bashrc
-echo "alias ecn=emacsclient -n -a ''" >> ~/.bashrc
+echo "alias ec=\"emacsclient -n -c -a ''\"" >> ~/.bashrc
+echo "alias ecn=\"emacsclient -n -a ''\"" >> ~/.bashrc
