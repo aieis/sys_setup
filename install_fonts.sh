c58932fefee57dccab723a17162e3a9f2fdc0dcf
Ahmed Salem <aieisalem17@gmail.com>
2023-04-21 17:31:01 +0100

    Add install fonts

---
+++ install_fonts.sh
@@ -0,0 +1,5 @@
+wget https://github.com/be5invis/Iosevka/releases/download/v22.0.2/super-ttc-iosevka-22.0.2.zip -O iosevka.zip
+mkdir fonts
+unzip iosevka -d fonts
+mkdir -p ~/.local/share/fonts
+fc-cache
